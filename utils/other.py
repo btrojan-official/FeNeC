792c6ac594f69bdab703a7eca4ec36cfeecf1b57
MrFishPL <karpik-junior@o2.pl>
2025-01-22 10:48:42 +0100

    some changes in requirements and utils

--- utils/other.py
+++ utils/other.py
@@ -41,27 +41,57 @@ def _get_smallest_values_per_class(data, labels, n_smallest):
 
     return result
 
-def load_data(task_num=0, dataset_name="resnet",load_covariances=False, load_prototypes=False):
-    current_file = f"./data/{dataset_name}/task_{task_num}.hdf5"
 
-    with h5py.File(current_file, "r") as f:
-        X_train = f["X_train"][:]
-        y_train = f["y_train"][:]
-        covariances = f["covariances"][:]
-        X_test = f["X_test"][:]
-        y_test = f["y_test"][:]
-        test_predictions = f["test_predictions"][:]
-        task_id = f["info"].attrs["task"]
-        accuracy = f["info"].attrs["accuracy"]
-        if load_prototypes:
-            prototypes = f["prototypes"][:]
+class GradKNNDataloader:
+    def __init__(self, num_tasks=6, dataset_name="resnet", load_covariances=False, load_prototypes=False):
+        """
+        This DataLoader class loads all tasks at once so they can be reused multiple times
+        without re-reading from disk.
+        """
+        self.dataset_name = dataset_name
+        self.num_tasks = num_tasks
+        self.load_covariances = load_covariances
+        self.load_prototypes = load_prototypes
+        self.data = []
 
-        print(f"Accuracy: {accuracy}, Task ID: {task_id}")
+        # Load all tasks into memory during initialization
+        for task_num in range(self.num_tasks):
+            current_file = f"./data/{self.dataset_name}/task_{task_num}.hdf5"
+            with h5py.File(current_file, "r") as f:
+                X_train = f["X_train"][:]
+                y_train = f["y_train"][:]
+                covariances = f["covariances"][:]
+                X_test = f["X_test"][:]
+                y_test = f["y_test"][:]
 
-        if load_covariances and load_prototypes:
-            return torch.tensor(X_train), torch.tensor(y_train), torch.tensor(X_test), torch.tensor(y_test), torch.tensor(covariances), torch.tensor(prototypes)
-        if load_covariances:
-            return torch.tensor(X_train), torch.tensor(y_train), torch.tensor(X_test), torch.tensor(y_test), torch.tensor(covariances)
-        if load_prototypes:
-            return torch.tensor(X_train), torch.tensor(y_train), torch.tensor(X_test), torch.tensor(y_test), torch.tensor(prototypes)
-        return torch.tensor(X_train), torch.tensor(y_train), torch.tensor(X_test), torch.tensor(y_test)
+                # If prototypes are needed, load them; otherwise set None
+                if self.load_prototypes:
+                    prototypes = f["prototypes"][:]
+                else:
+                    prototypes = None
+
+                # Store loaded data in a list of dictionaries
+                self.data.append({
+                    "X_train": torch.tensor(X_train),
+                    "y_train": torch.tensor(y_train),
+                    "X_test": torch.tensor(X_test),
+                    "y_test": torch.tensor(y_test),
+                    "covariances": torch.tensor(covariances),
+                    "prototypes": torch.tensor(prototypes) if prototypes is not None else None
+                })
+
+    def get_data(self, task_num):
+        """
+        Returns a tuple (X_train, y_train, X_test, y_test, covariances, prototypes) 
+        for the specified task number.
+        """
+        X_train = self.data[task_num]["X_train"]
+        y_train = self.data[task_num]["y_train"]
+        X_test = self.data[task_num]["X_test"]
+        y_test = self.data[task_num]["y_test"]
+
+        # Retrieve covariances and prototypes only if they were loaded
+        covariances = self.data[task_num]["covariances"] if self.load_covariances else None
+        prototypes = self.data[task_num]["prototypes"] if self.load_prototypes else None
+
+        return X_train, y_train, X_test, y_test, covariances, prototypes
