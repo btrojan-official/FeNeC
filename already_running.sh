be2681c192904ff7fe73012780a8ed5dababce96
MrFishPL <karpik-junior@o2.pl>
2025-01-22 13:09:01 +0100

    preparing for experiments

--- already_running.sh
+++ already_running.sh
@@ -31,3 +31,35 @@ python search.py \
     --output_file searches_results/cifar100_vit_no_logits_QMCSampler.csv \
     --sampler QMCSampler \
     --result_dir searches_results/cifar100_vit_no_logits_QMCSampler
+
+# ResNet Cifar 100 no logits
+
+python search.py \
+    --dataset resnet \
+    --model resnet \
+    --use_logits false \
+    --num_of_trials 600 \
+    --num_of_tasks 6 \
+    --output_file searches_results/cifar100_resnet_no_logits_TPESampler.csv \
+    --sampler TPESampler \
+    --result_dir searches_results/cifar100_resnet_no_logits_TPESampler
+
+python search.py \
+    --dataset resnet \
+    --model resnet \
+    --use_logits false \
+    --num_of_trials 600 \
+    --num_of_tasks 6 \
+    --output_file searches_results/cifar100_resnet_no_logits_GPSampler.csv \
+    --sampler GPSampler \
+    --result_dir searches_results/cifar100_resnet_no_logits_GPSampler
+
+python search.py \
+    --dataset resnet \
+    --model resnet \
+    --use_logits false \
+    --num_of_trials 600 \
+    --num_of_tasks 6 \
+    --output_file searches_results/cifar100_resnet_no_logits_QMCSampler.csv \
+    --sampler QMCSampler \
+    --result_dir searches_results/cifar100_resnet_no_logits_QMCSampler
--- to_run.sh
+++ to_run.sh
@@ -1,38 +1,5 @@
 ###### NO LOGITS ######
 
-# ResNet Cifar 100 no logits
-
-python search.py \
-    --dataset resnet \
-    --model resnet \
-    --use_logits false \
-    --num_of_trials 600 \
-    --num_of_tasks 6 \
-    --output_file searches_results/cifar100_resnet_no_logits_TPESampler.csv \
-    --sampler TPESampler \
-    --result_dir searches_results/cifar100_resnet_no_logits_TPESampler
-
-python search.py \
-    --dataset resnet \
-    --model resnet \
-    --use_logits false \
-    --num_of_trials 600 \
-    --num_of_tasks 6 \
-    --output_file searches_results/cifar100_resnet_no_logits_GPSampler.csv \
-    --sampler GPSampler \
-    --result_dir searches_results/cifar100_resnet_no_logits_GPSampler
-
-python search.py \
-    --dataset resnet \
-    --model resnet \
-    --use_logits false \
-    --num_of_trials 600 \
-    --num_of_tasks 6 \
-    --output_file searches_results/cifar100_resnet_no_logits_QMCSampler.csv \
-    --sampler QMCSampler \
-    --result_dir searches_results/cifar100_resnet_no_logits_QMCSampler
-
-
 # ResNet Tiny ImageNet no logits
 
 python search.py \
