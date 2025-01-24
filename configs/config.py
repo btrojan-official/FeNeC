53772180c83eb71afdd7c0104932c3b9411445db
btrojan-official <bartosztrojanofficial@gmail.com>
2025-01-24 13:25:48 +0100

    testing run with params from second implementation - everything as expected

--- configs/config.py
+++ configs/config.py
@@ -2,20 +2,20 @@ config = {
     "metric": "mahalanobis", # ["euclidean", "mahalanobis"]
     "weight": "uniform", # ["uniform", "distance"]
 
-    "tukey_lambda": 0.5,
+    "tukey_lambda": 0.4,
     "num_of_shrinkages": 2,
-    "shrinkage_alpha_0": 1,
-    "shrinkage_alpha_1": 1,
+    "shrinkage_alpha_0": 0.85,
+    "shrinkage_alpha_1": 1.55,
 
     "norm_in_mahalanobis": True,
 
-    "knn_k": 1,
+    "knn_k": 3,
 
     "use_kmeans": True,
-    "kmeans_k": 12,
+    "kmeans_k": 5,
     "sklearn_seed": 42,
 
-    "use_logits_mode_0": True,
+    "use_logits_mode_0": False,
     "logits_n_samples": 6,
     "logits_train_epochs":50,
     "logits_batch_size": 64,
