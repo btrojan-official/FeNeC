d9e9af075c5380dbca9bee11b7cd75d0e9ba1739
btrojan-official <bartosztrojanofficial@gmail.com>
2025-03-01 10:35:21 +0100

    generated task_vs_accuracy pdf plots

--- utils/draw_plot.py
+++ utils/draw_plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
 def plot_method_scores(df: pd.DataFrame, title: str) -> None:
     """
@@ -19,7 +20,10 @@ def plot_method_scores(df: pd.DataFrame, title: str) -> None:
     }).reset_index()
     grouped.columns = ["method_name", "task", "mean_accuracy", "std_accuracy"]
     
-    plt.figure(figsize=(6, 4))
+    sns.set_context("paper", font_scale=1.6)
+    sns.set_style("whitegrid")
+
+    plt.figure(figsize=(7, 5))
     
     for method in grouped["method_name"].unique():
         method_data = grouped[grouped["method_name"] == method]
@@ -29,11 +33,16 @@ def plot_method_scores(df: pd.DataFrame, title: str) -> None:
                          method_data["mean_accuracy"] + method_data["std_accuracy"],
                          alpha=0.2)
     
-    plt.xlabel("Task number")
-    plt.ylabel("Accuracy")
-    plt.title(title)
+    plt.xlabel("Task number", fontsize=19.2)
+    plt.ylabel("Accuracy", fontsize=19.2)
+    plt.title(title, fontsize=22.4)
     plt.legend(title="Methods")
     plt.grid(True, linestyle='--', alpha=0.6)
+
+    plt.tight_layout()
+    if os.path.exists(os.path.join("hyperparameters_selection", "plots")) == False:
+        os.makedirs(os.path.join("hyperparameters_selection", "plots"))
+    plt.savefig(os.path.join("hyperparameters_selection", "plots", title + ".pdf"), format="pdf", bbox_inches="tight")
     
     # Show plot
     plt.show()
