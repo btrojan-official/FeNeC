24a6a2380ce5155781dda553f4a307ff162c677d
btrojan-official <bartosztrojanofficial@gmail.com>
2025-03-05 14:41:21 +0100

    drawing more accurate charts

--- utils/draw_plot.py
+++ utils/draw_plot.py
@@ -12,6 +12,11 @@ def plot_method_scores(df: pd.DataFrame, title: str) -> None:
         df (pd.DataFrame): A dataframe containing method names and their scores across different tasks.
     """
 
+    markers = ["d", "o", "*"]
+    linestyles= ["solid", "solid", "solid"]
+    # colors = ["#FF9F1C", "#2B9348", "#0077B6"]
+    alpha = 0.6
+
     df_long = df.melt(id_vars=["method_name"], var_name="task", value_name="accuracy")
     df_long["task"] = df_long["task"].astype(int)
     
@@ -23,11 +28,13 @@ def plot_method_scores(df: pd.DataFrame, title: str) -> None:
     sns.set_context("paper", font_scale=1.6)
     sns.set_style("whitegrid")
 
-    plt.figure(figsize=(7, 5))
+    plt.figure(figsize=(6, 4))
     
-    for method in grouped["method_name"].unique():
+    methods_names = ["FeCAM", "FeNeC", "FeLoC"]
+
+    for i, method in enumerate(methods_names):
         method_data = grouped[grouped["method_name"] == method]
-        plt.plot(method_data["task"], method_data["mean_accuracy"], marker='o', label=method, linewidth=2, markersize=8)
+        plt.plot(method_data["task"], method_data["mean_accuracy"], marker=markers[i], linestyle=linestyles[i], label=method, linewidth=2, markersize=8, alpha=alpha) # , color=colors[i]
         plt.fill_between(method_data["task"], 
                          method_data["mean_accuracy"] - method_data["std_accuracy"],
                          method_data["mean_accuracy"] + method_data["std_accuracy"],
@@ -40,7 +47,7 @@ def plot_method_scores(df: pd.DataFrame, title: str) -> None:
     plt.xlabel("Task number", fontsize=19.2)
     plt.ylabel("Accuracy (%)", fontsize=19.2)
     plt.title(title, fontsize=22.4)
-    plt.legend(title="Methods", fontsize=18)
+    plt.legend(title="Methods", fontsize=17.5)
     plt.grid(True, linestyle='--', alpha=0.6)
 
     plt.tight_layout()
